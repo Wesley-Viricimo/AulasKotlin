999c14e02d3b501a70b5445b3b2525b75213c4f2
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-12 14:21:54 -0300

    Exercicio utilizando variáveis

---
+++ src/main/kotlin/sessao5/exercicioVariaveis/ExercicioVariaveis.kt
@@ -0,0 +1,13 @@
+package sessao5.exercicioVariaveis
+
+fun main() {
+    var nome: String = "Wesley Viricimo Silva"
+    var textSemValor: String
+
+    val calcado: Byte = 40
+
+    val pib: Long = 1890000000000
+    val populacao: Int = 211600038
+
+    print("R$ ${pib/populacao}")
+}
