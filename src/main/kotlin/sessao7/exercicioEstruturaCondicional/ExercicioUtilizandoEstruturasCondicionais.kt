177226af61af29470d209aaa070bf3fbdac3632b
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-12 17:02:58 -0300

    Operador condicional when

--- src/main/kotlin/sessao7/exercicioEstruturaCondicional/ExercicioUtilizandoEstruturasCondicionais.kt
+++ src/main/kotlin/sessao7/exercicioEstruturaCondicional/ExercicioUtilizandoEstruturasCondicionais.kt
@@ -6,6 +6,9 @@ fun main() {
 
     val bonus2 = cargo("Gerente", 2);
     println(bonus2)
+
+    val bonusWhen = bonusWhen("Estagiário");
+    println(bonusWhen)
 }
 
 fun cargo(cargo: String): Float {
@@ -40,6 +43,26 @@ fun cargo(cargo: String, experiencia: Int): Float {
         bonus = 1000f
     } else if (cargo == "Estagiário") {
         bonus = 500f
+    }
+    return bonus
+}
+
+fun bonusWhen(cargo: String) : Float {
+    /*var bonus = 0f;
+    when (cargo) { //Operador when é similar ao switch
+        "Gerente" -> bonus = 2000f
+        "Coordenador" -> bonus = 1500f
+        "Engenheiro de software" -> bonus = 1000f
+        "Estagiário" -> bonus = 500f
+    }
+    return bonus*/
+
+    val bonus = when(cargo) {
+        "Gerente" -> 2000f
+        "Coordenador" -> 1500f
+        "Engenheiro de software" -> 1000f
+        "Estagiário" -> 500f
+        else -> 0f //Se o cargo não for nehum retornará 0
     }
     return bonus
 }
