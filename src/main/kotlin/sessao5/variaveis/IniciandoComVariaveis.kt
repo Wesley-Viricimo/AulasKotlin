7b6e5d1e9531cb28499c624387140be9b90c3015
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-12 11:39:21 -0300

    Iniciando com variáveis

---
+++ src/main/kotlin/sessao5/variaveis/IniciandoComVariaveis.kt
@@ -0,0 +1,10 @@
+package sessao5.variaveis
+
+class IniciandoComVariaveis {
+    fun main() {
+        var nome: String = "Wesley"
+        var idade: Int = 26
+
+        println("Nome " +  nome + " Idade " + idade + " Anos")
+    }
+}
