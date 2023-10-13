3731b553d7aa47513a921a06334e0ad51c164d2f
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-12 21:26:56 -0300

    Laço de repetição for

---
+++ src/main/kotlin/sessao7/estruturaRepeticao/EstruturaRepeticaoFor.kt
@@ -0,0 +1,11 @@
+package sessao7.estruturaRepeticao
+
+fun main() {
+    for (i in 0..10 step 2) { //Percorrendo a lista de 2 em 2
+        println("$i ")
+    }
+
+    for (i in 20 downTo 0 step 2) {//Percorrendo a lista em ordem decrescente de 2 em 2
+        println("$i ")
+    }
+}
