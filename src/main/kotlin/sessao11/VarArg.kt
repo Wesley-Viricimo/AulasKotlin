72cd927c79249c1637026a099563e00f4ec9a480
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-14 19:14:25 -0300

    Mais sobre vararg e generics

--- src/main/kotlin/sessao11/VarArg.kt
+++ src/main/kotlin/sessao11/VarArg.kt
@@ -1,7 +1,8 @@
 package sessao11
 
 fun main() {
-    print(media(1f, 2f, 3f, 4f))
+    println(media(1f, 2f, 3f, 4f))
+    println(mediaGenerics(false, 1f, 2f, false, ""))
 }
 
 fun media(vararg notas: Float) : Float { //Vararg permite que sejam enviados vários valores como parâmetro desde que possuam o mesmo tipo
@@ -9,6 +10,17 @@ fun media(vararg notas: Float) : Float { //Vararg permite que sejam enviados vá
 
     for (n in notas) {
         soma += n
+    }
+    return (soma / notas.size)
+}
+
+fun <T, J> mediaGenerics(abc: J, vararg notas: T) : Float { //Função que utilizando generics, desta forma poderá ser passado como argumento valores de qualquer tipo e serão aceitos. VarArg sempre deve ser último parâmtro recebido
+    var soma = 0f
+
+    for (n in notas) {
+        if (n is Float) { //Verifica se o valor de n é um float
+            soma += n
+        }
     }
     return (soma / notas.size)
 }
