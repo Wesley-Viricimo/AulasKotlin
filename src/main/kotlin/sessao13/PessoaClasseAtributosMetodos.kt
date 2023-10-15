0682e2a5a6b091f4b4499721dda6d2bb8dd56ae5
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-14 21:47:40 -0300

    Construtores e this

--- src/main/kotlin/sessao13/PessoaClasseAtributosMetodos.kt
+++ src/main/kotlin/sessao13/PessoaClasseAtributosMetodos.kt
@@ -1,6 +1,11 @@
 package sessao13
 
 class PessoaClassesAtributosMetodos(var anoNascimento: Int, var nome: String) { //Precisa ser declarado o var para o atributo ser acessado fora da classe
+    var doc: String? = null
+    constructor(anoNascimento: Int, nome: String, doc: String) : this(anoNascimento, nome) {
+        this.doc = doc
+    }
+
     fun dormir() {
 
     }
@@ -12,12 +17,13 @@ class PessoaClassesAtributosMetodos(var anoNascimento: Int, var nome: String) { 
 
 fun main() {
     //Instância do objeto pessoa
-    var pessoa: PessoaClassesAtributosMetodos = PessoaClassesAtributosMetodos(1997, "Wesley")
+    var pessoa: PessoaClassesAtributosMetodos = PessoaClassesAtributosMetodos(1997, "Wesley", "2112345")
 
     println(pessoa.nome)
     println(pessoa.anoNascimento)
 
     pessoa.dormir()
     pessoa.acordar()
+    pessoa.doc
 
 }
