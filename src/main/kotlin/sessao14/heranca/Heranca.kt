46570715a5a1529a3efa1b159fd392dd496d115c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-15 22:21:44 -0300

    Override e super

--- src/main/kotlin/sessao14/heranca/Heranca.kt
+++ src/main/kotlin/sessao14/heranca/Heranca.kt
@@ -1,15 +1,27 @@
 package sessao14.heranca
 
 open class Eletronico(private var marca: String) { //Para que outras classes possam herdar desta é necessário indicar que é uma classe aberta
-   private fun ativarCorrente(){}//Método não será enxergue fora da classe
+   private fun corrente(ativo: Boolean){}//Método não será enxergue fora da classe
     fun ligar() {
-        ativarCorrente()
+        corrente(true)
     }
-    fun desligar() {}
+    open fun desligar() {
+        corrente(false)
+    }
 }
 
 class Computador(marca: String) : Eletronico(marca) { //Herdando da classe Eletronico
+    fun save() {
 
+    }
+
+    /*Como já existe um método com nome desligar na classe pai, porém não se comporta da forma que preciso
+    sobrescrevi com override o método desligar
+    */
+    override fun desligar() {
+        save()
+        super.desligar() //Super serve para ser chamado o método desligar da classe pai
+    }
 }
 
 fun main() {
@@ -17,6 +29,6 @@ fun main() {
 
     c.ligar()
     c.desligar()
-    //c.ativarCorrente()
+    //c.corrente()
     //c.marca
 }
