5cdac4c9f613bac565244f8112ac632d4c0f0a78
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-16 21:35:24 -0300

    Set

---
+++ src/main/kotlin/sessao15/colecoes/Colecoes.kt
@@ -0,0 +1,13 @@
+package sessao15.colecoes
+
+fun main() {
+    val list : List<Int> = listOf(1,2,3,4,5) //Na lista do kotlin não é possível que seja adicionado valores
+    val list2 : MutableList<Int> = mutableListOf(1,2,3,4,5) //Na mutableList é possível adicionar valores
+
+    val set1 : Set<String> = setOf<String>() //Coleções do tipo set não permite valores duplicados, não irá dar erro porém não irá duplicar e também não permite que sejam adicionados valores
+    val set2 : MutableSet<String> = mutableSetOf<String>()
+
+
+
+
+}
--- src/main/kotlin/sessao15/set/Set.kt
+++
@@ -1,8 +0,0 @@
-package sessao15.set
-
-fun main() {
-    val list : List<Int> = listOf(1,2,3,4,5) //Na lista do kotlin não é possível que seja adicionado valores
-    val list2 : MutableList<Int> = mutableListOf(1,2,3,4,5) //Na mutableList é possível adicionar valores
-
-
-}
