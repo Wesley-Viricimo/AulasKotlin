cb788e0ec7a41b33ab156069f3e86eacd4a9d19f
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-16 21:44:55 -0300

    Map

--- src/main/kotlin/sessao15/colecoes/Colecoes.kt
+++ src/main/kotlin/sessao15/colecoes/Colecoes.kt
@@ -7,6 +7,20 @@ fun main() {
     val set1 : Set<String> = setOf<String>() //Coleções do tipo set não permite valores duplicados, não irá dar erro porém não irá duplicar e também não permite que sejam adicionados valores
     val set2 : MutableSet<String> = mutableSetOf<String>()
 
+    val map1 = mapOf<String, String>(Pair("França", "Paris"), Pair("Alemanha", "Berlim"))
+    val map2 = mutableMapOf<String, String>()
+
+    println(map1.entries) //Chaves e valores
+    println(map1.keys)    //Chaves
+    println(map1.values)  //Valores
+
+    map2.put("Brasil", "Brasilia")
+    map2.put("Angola", "Moçambique")
+
+    println(map2.entries) //Chaves e valores
+    println(map2.keys)    //Chaves
+    println(map2.values)  //Valores
+
 
 
 
