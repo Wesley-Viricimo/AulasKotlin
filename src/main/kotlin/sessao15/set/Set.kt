7490673628318d3dd7f58f2706286b288dd4227c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-16 21:26:02 -0300

    List of e mutable list of

---
+++ src/main/kotlin/sessao15/set/Set.kt
@@ -0,0 +1,8 @@
+package sessao15.set
+
+fun main() {
+    val list : List<Int> = listOf(1,2,3,4,5) //Na lista do kotlin não é possível que seja adicionado valores
+    val list2 : MutableList<Int> = mutableListOf(1,2,3,4,5) //Na mutableList é possível adicionar valores
+
+
+}
