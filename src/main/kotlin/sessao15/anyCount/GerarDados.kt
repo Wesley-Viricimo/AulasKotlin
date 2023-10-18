de5609c3c79f9ca47078d7af355e5200cc277eb9
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-17 21:03:32 -0300

    Foreach

--- src/main/kotlin/sessao15/anyCount/GerarDados.kt
+++ src/main/kotlin/sessao15/anyCount/GerarDados.kt
@@ -3,19 +3,21 @@ package sessao15.anyCount
 fun main() {
     val data = gerarDados()
 
+    data.forEach { println(it.nome) } //Percorrendo todos os itens da lista e exibindo o nome das receitas
+
     //data.any() -> Retorna true se a coleção tiver ao menos um elemento
     //data.count() -> Retorna a quantidade de elementos na coleção
 
-    println("Tenho dados? ${if(data.any()) "Sim" else "Não"}")
-    println("Tenho  ${data.count()} elementos")
+    //println("Tenho dados? ${if(data.any()) "Sim" else "Não"}")
+    //println("Tenho  ${data.count()} elementos")
 
-    println("Primeira receita: ${data.first()}") //Pega primeiro item da lista
-    println("Ultima receita: ${data.last()}") //Pega ultimo item da lista
+    //println("Primeira receita: ${data.first()}") //Pega primeiro item da lista
+    //println("Ultima receita: ${data.last()}") //Pega ultimo item da lista
 
-    println("Receita: ${data.filter { it.nome == "Lasanha" }}") //Busca na lista algum elemento onde o nome seja igual a lasanha
+    //println("Receita: ${data.filter { it.nome == "Lasanha" }}") //Busca na lista algum elemento onde o nome seja igual a lasanha
 
-    data.take(2) //Pega os 2 primeiros itens da lista
-    data.takeLast(2) //Pega os 2 ultimos itens da lista
+    //data.take(2) //Pega os 2 primeiros itens da lista
+    //data.takeLast(2) //Pega os 2 ultimos itens da lista
 }
 
 fun gerarDados(): List<Receita> {
